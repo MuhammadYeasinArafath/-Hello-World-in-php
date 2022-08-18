7154729db16962855df12468b8c15eb0054a34a4
MuhammadYeasinArafath <mdyeasinarafathofficial@gmail.com>
2022-08-19 02:37:49 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# -Hello-World-in-php
+“Hello, World!” in php.
+<br>
+.php file. run with XAMPP. install XAMPP in your device & launch the server.
+<br>
+XAMPP download link: https://www.apachefriends.org/download.html
+<br>
+download the repo & place it in C:\xampp\htdocs [this is your php file memery. here we will place the php file.]
+<br>
+go to http://localhost/yourWebApplicationName
+<br>
+100% working code.
+<br>
+github pages do not support php file. thats why we need XAMPP & localhost.
---
+++ hello.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>“Hello, World!” in php</title>
+    <link rel="stylesheet" href="./index.css">
+</head>
+<body>
+    
+   <h2>“Hello, World!” in php</h2>
+<br>
+<br>
+<br>
+  <!--php code here -->
+  <?php
+        echo "Hello, World!";
+  ?>
+  <br><br><br>
+  <?php
+        echo "“Hello, World!” in php";
+  ?>
+
+
+
+</body>
+</html>
